d0c1de6e1c62980e2ae21f64241c8d172c20edd4
koleks92 <koleks92@gmail.com>
2023-09-27 10:42:16 +0200

    SignUp with Firebase

--- util/auth.js
+++ util/auth.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_KEY = 'AIzaSyCINoC841nNLdFuBrMBN_AiEXvkEeBOTgg'
 
-async function createUser(email, password, ) {
-    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=' + API_KEY,
+export async function createUser(email, password) {
+    const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
         email: email,
         password: password,
